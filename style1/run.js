cd0ff1b0920f85f0920fa25aa49667091c8e4e6f
zhangchengmcbedrock <120537551+zhangchengmcbedrock@users.noreply.github.com>
2023-06-13 21:33:55 +0800

    613

---
+++ style1/run.js
@@ -0,0 +1,9 @@
+document.addEventListener("keydown", function(event) {
+    if (event.key === "1") {
+      window.location.href = "https://www.baidu.com";
+    } else if (event.key === "2") {
+      window.location.href = "https://www.bing.com";
+    } else if (event.key === "3") {
+      window.location.href = "https://www.google.com/intl/zh-CN";
+    }
+  });

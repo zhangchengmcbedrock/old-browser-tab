18309ddc7b6a2e477b92b9605ef95212dcd525cd
zhangchengmcbedrock <120537551+zhangchengmcbedrock@users.noreply.github.com>
2023-07-11 22:28:31 +0800

    1

--- style1/test/run.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("keydown", function(event) {
-    if (event.key === "1") {
-      window.location.href = "https://www.baidu.com";
-    } else if (event.key === "2") {
-      window.location.href = "https://www.bing.com";
-    } else if (event.key === "3") {
-      window.location.href = "https://www.google.com/intl/zh-CN";
-    }
-  });
